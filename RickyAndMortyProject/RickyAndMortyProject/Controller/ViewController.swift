14697bed7f593aa49de03eb56c91655a79fe8b00
Robson Novato <rnlobao42@gmail.com>
2021-07-07 12:11:40 -0300

    Requisição de characters na api

--- RickyAndMortyProject/RickyAndMortyProject/Controller/ViewController.swift
+++ RickyAndMortyProject/RickyAndMortyProject/Controller/ViewController.swift
@@ -34,6 +34,7 @@ extension RickAndMortyViewController: UITableViewDelegate,UITableViewDataSource 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuse) as! CharacterTableViewCell
+        cell.setupCell(user: <#T##Character#>)
         return cell //Ela fala qual celula ela vai exibir, no caso é o xib
     }
     
--- RickyAndMortyProject/RickyAndMortyProject/View/Xibs/CharacterTableViewCell.swift
+++ RickyAndMortyProject/RickyAndMortyProject/View/Xibs/CharacterTableViewCell.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import Nuke
 
 class CharacterTableViewCell: UITableViewCell {
 
     @IBOutlet weak var iconImageView: UIImageView!
-    
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var especieLabel: UILabel!
+    @IBOutlet weak var specieLabel: UILabel!
     
+    
+    func setupCell(user: Character) {
+            nameLabel.text = user.name ?? ""
+            specieLabel.text = user.species ?? ""
+            if let url = URL(string: user.image ?? "") {
+                Nuke.loadImage(with: ImageRequest(url: url), into: iconImageView)
+            }
+        }
 }
