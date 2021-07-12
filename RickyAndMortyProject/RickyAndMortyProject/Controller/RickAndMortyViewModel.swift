a3516bf352cb6abfbd50cf236b3582a386a9ee08
Robson Novato <rnlobao42@gmail.com>
2021-07-12 11:57:35 -0300

    Request de dados e setup da celula

--- RickyAndMortyProject/RickyAndMortyProject/Controller/RickAndMortyViewModel.swift
+++ RickyAndMortyProject/RickyAndMortyProject/Controller/RickAndMortyViewModel.swift
@@ -7,7 +7,35 @@
 
 import Foundation
 
-class RickAndMortyViewModel {
-    //chama o service e retorna os dados
+protocol RickAndMortyDelegate {
+    func getDataSucess()
+    func getDataFail(error: Error)
+    func showLoad()
+    func removeLoad()
+}
+
+class RickAndMortyViewModel  {
+    
+    var service: CharacterService?
+    var characters: [Character] = []
+    var delegate: RickAndMortyDelegate?
+    
+    init(_ service: CharacterService? = CharacterService(), delegate: RickAndMortyDelegate? = nil) {
+        self.service = service
+        self.delegate = delegate
+    }
+    
+    
+    func getData() {
+        delegate?.showLoad()
+        service?.getCharacters(sucess: { result in
+            self.characters = result
+            self.delegate?.getDataSucess()
+            self.delegate?.removeLoad()
+        }, error: { error in
+            self.delegate?.getDataFail(error: error)
+            self.delegate?.removeLoad()
+        })
+    }
     
 }
--- RickyAndMortyProject/RickyAndMortyProject/Controller/ViewController.swift
+++ RickyAndMortyProject/RickyAndMortyProject/Controller/ViewController.swift
@@ -14,9 +14,13 @@ class RickAndMortyViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var viewModel : RickAndMortyViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = RickAndMortyViewModel(delegate: self)
         setupTableView()
+        viewModel.getData()
     }
     
     func setupTableView() {
@@ -29,12 +33,12 @@ class RickAndMortyViewController: UIViewController {
 extension RickAndMortyViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10 //quantas vezes vai rodar a celula
+        return viewModel.characters.count //quantas vezes vai rodar a celula
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuse) as! CharacterTableViewCell
-        cell.setupCell(user: <#T##Character#>)
+        cell.setupCell(user: viewModel.characters[indexPath.row])
         return cell //Ela fala qual celula ela vai exibir, no caso é o xib e como vao ser suas configuracoes
     }
     
@@ -45,4 +49,27 @@ extension RickAndMortyViewController: UITableViewDelegate,UITableViewDataSource 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true) //ao clicar acontece isso
     }
+}
+
+
+extension RickAndMortyViewController: RickAndMortyDelegate {
+    func getDataSucess() {
+        tableView.reloadData()
+    }
+    
+    func getDataFail(error: Error) {
+        let alert = UIAlertController(title: "Erro", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .destructive, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    func showLoad() {
+        showSpinner(onView: view)
+    }
+    
+    func removeLoad() {
+        removeSpinner()
+    }
+    
+    
 }
