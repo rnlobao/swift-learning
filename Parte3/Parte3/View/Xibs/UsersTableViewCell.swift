873f7b8affd11855de4a4caf5db859428b805c56
Robson Novato <rnlobao42@gmail.com>
2021-07-23 14:58:06 -0300

    ...

--- Parte3/Parte3/View/Xibs/UsersTableViewCell.swift
+++ Parte3/Parte3/View/Xibs/UsersTableViewCell.swift
@@ -11,13 +11,14 @@ import Nuke
 class UsersTableViewCell: UITableViewCell {
 
     @IBOutlet weak var iconImageView: UIImageView!
-    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var firstNameLabel: UILabel!
+    
     
     
     func setupCell(user: Datum) {
-            nameLabel.text = user.firstName ?? ""
-            emailLabel.text = user.email ?? ""
+        firstNameLabel.text = user.firstName ?? ""
+        emailLabel.text = user.email ?? ""
         if let url = URL(string: user.avatar ?? "") {
                 Nuke.loadImage(with: ImageRequest(url: url), into: iconImageView)
             }
