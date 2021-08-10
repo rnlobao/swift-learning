0f3a1eb17c823dd10adec46a957f2676ff1de6f7
Robson Novato <rnlobao42@gmail.com>
2021-08-10 17:20:08 -0300

    Init(User Default Practicing)

---
+++ UserDefault Register App/UserDefault Register App/Controller/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UserDefault Register App
+//
+//  Created by Robson Novato Lobao on 10/08/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
---
+++ UserDefault Register App/UserDefault Register App/Controller/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  UserDefault Register App
+//
+//  Created by Robson Novato Lobao on 10/08/21.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var registerEmailTextField: UITextField!
+    @IBOutlet weak var registerPasswordTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBAction func saveButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        <#code#>
+    }
+}
