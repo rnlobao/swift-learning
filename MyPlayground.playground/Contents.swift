bb76050bfdcc0aa0e1781bd81efb90b0cd11c386
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2021-12-20 09:26:15 -0300

    .

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,20 @@
-var a: String
-if a == "" {
-    print("oi")
+func compositionCheck(input: String) -> Bool {
+    var firstName = 0
+    var secondName = -1
+    var isItSecondName = false
+        for chr in input {
+            if chr == " " || isItSecondName == true {
+                secondName += 1
+                isItSecondName = true
+            }
+            else {
+                firstName += 1
+            }
+        }
+    if firstName > 1 && secondName > 1 {
+        return true
+    }
+    return false
 }
+
+compositionCheck(input: "aa aa")
