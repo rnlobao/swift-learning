83942f98582691f515ac59536c317c3969d8357a
Robson Novato <66230142+rnlobao@users.noreply.github.com>
2022-02-07 15:58:08 -0300

    .

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,7 @@
+var a = "á"
+a.unicodeScalars.first?.value
 
-var email = "robson@gmail.com"
-var count = 0
-var whereisarroba = 0
-for n in email {
-    if n == "@" {
-        whereisarroba = count
-    }
-    count += 1
-}
-print(whereisarroba)
-var letra = email[email.index(email.startIndex, offsetBy: whereisarroba)]
+
 
 
 
